072676563ed1f6e43166c711c1659f599546a3c5
Fabri <fabricioclaudio1@gmail.com>
2025-05-21 17:08:21 -0300

    Ejercicio 1 realizado

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+Console.WriteLine("valor de b:" + b);
+
+
+//Ejercicio 1
+string numeroString = Console.ReadLine();
+int numero;
+
+bool resultado = int.TryParse(numeroString, out numero);
+
+if (resultado)
+{
+    float num = (float)1 / numero;
+    Console.WriteLine($"1/{numero}");
+    Console.WriteLine(num);
+}
+else
+{
+    Console.WriteLine("Lo que ingreso no es un numero.");
+}
