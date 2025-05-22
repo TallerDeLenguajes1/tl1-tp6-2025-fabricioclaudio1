a6d83310271241a9be1fa241a67b3febd8aa24d9
Fabri <fabricioclaudio1@gmail.com>
2025-05-22 14:11:37 -0300

    CalculadoraV2 realizada

--- Program.cs
+++ Program.cs
@@ -40,15 +40,58 @@ La parte entera de un tipo float: {(int)numero}
 //Segunda Parte de Calculadora v2
 Console.WriteLine("Ingrese dos numeros: ");
 
-int numeroA = IngresarNumero();
-Console.WriteLine(@$"Numero ingresado: {numeroA} 
-Ingrese el otro
+int Minimo = IngresarNumero();
+Console.WriteLine(@$"
+Numero ingresado: {Minimo} 
+Ingrese el otro numero: 
 ");
+int Maximo = IngresarNumero();
+Console.WriteLine(@$"
+Numero ingresado: {Maximo} 
+");
+
+int aux = 0;
+if (Minimo > Maximo)
+{
+    Minimo = aux;
+    Minimo = Maximo;
+    Maximo = aux;
+}
+
+int longitud = Maximo - Minimo;
+if (longitud == 0)
+{
+    Console.WriteLine(@$"
+Numero Maximo y Minimo es: {Maximo} 
+");
+}
+else if (longitud == 1)
+{
+    Console.WriteLine(@$"
+Numero Maximo es: {Maximo} 
+Numero Minimo es: {Minimo} 
+");
+}
+else if (longitud == 2)
+{
+    Console.WriteLine(@$"
+Numero Maximo y Minimo es: {Maximo - 1} 
+");
+}
+else if (longitud > 2)
+{
+    Console.WriteLine(@$"
+Numero Maximo es: {Maximo - 1} 
+Numero Minimo es: {Minimo + 1} 
+");
+}
+
+
 
 int IngresarNumero()
 {
     int select = 1;
-    
+
     while (select != 0)
     {
         numeroString = Console.ReadLine();
